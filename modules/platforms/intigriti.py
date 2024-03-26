c3be8eaeb2062ec3fa1f7cb565873aca88eee42b
JoeWalker12 <109469979+JoeWalker12@users.noreply.github.com>
2024-03-27 00:50:03 +0330

    Update intigriti.py

--- modules/platforms/intigriti.py
+++ modules/platforms/intigriti.py
@@ -21,8 +21,7 @@ def check_intigriti(tmp_dir, mUrl, first_time, db, config):
 #            logo = "https://cdn.vectorstock.com/i/1000x1000/73/49/404-error-page-not-found-miss-paper-with-white-vector-20577349.webp"  # URL to a default or generic logo
 #       logo = f"https://api.intigriti.com/file/api/file/{program['logoId']}"
         programURL = f"https://app.intigriti.com/{program['webLinks']['detail']}"
-        data = {"programName": programName, "programType": "", "programURL": programURL,
-                "logo": logo, "platformName": "Intigriti","isRemoved": False, "isNewProgram": False, "color": 10858237}
+        data = {"programName": programName, "programType": "", "programURL": programURL, "platformName": "Intigriti","isRemoved": False, "isNewProgram": False, "color": 10858237}
         dataJson = {"programName": programName,
                     "programURL": programURL, "programType": "", "scope": {}, "reward": {}}
         programKey = generate_program_key(programName, programURL)
